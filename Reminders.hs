1f2cf9c263d62cbca6a5a871c590e40be4780035
Hokutosei <jeanepaul@gmail.com>
2017-01-05 01:06:36 +0900

    update

--- Reminders.hs
+++ Reminders.hs
@@ -1,10 +1,16 @@
 module Reminders (setReminder) where
 import Control.Concurrent (threadDelay)
+import Data.Time (getCurrentTime)
+
+data Person = Person { name :: String, age :: Int }
 
 setReminder :: String -> String -> IO()
 setReminder timerName delay = do
+    let michael = Person "Michael" 25
+    putStrLn ": " ++ show michael
     let delayTime = read delay :: Int
-    print ("setting reminder! " ++ timerName ++ " for time " ++ (show delayTime))
+    print ("setting reminder! " ++ timerName ++ " for time " ++ show delayTime)
     threadDelay (10^6 * delayTime)
-    print ("reminder fire! for " ++ timerName)
+    time <- getCurrentTime
+    putStrLn ("reminder fire! for " ++ timerName ++ ": " ++ show time)
     return ()
